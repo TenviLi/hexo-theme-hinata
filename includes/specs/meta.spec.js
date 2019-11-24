effdc39e33f82e88b27f513c682cab163b59dd51
gylidian <gylidian@gmail.com>
2019-11-24 17:52:39 +0800

    feat(config): support site logo setting

--- includes/specs/meta.spec.js
+++ includes/specs/meta.spec.js
@@ -1,14 +1,20 @@
-const { doc, type, defaultValue } = require('../common/utils').descriptors;
+const { doc, type, defaultValue } = require("../common/utils").descriptors;
 
 module.exports = {
     favicon: {
-        [type]: 'string',
-        [doc]: 'Path or URL to the website\'s icon',
-        [defaultValue]: '/images/favicon.svg',
+        [type]: "string",
+        [doc]: "Path or URL to the website's icon",
+        [defaultValue]: "/images/favicon.ico"
     },
     rss: {
-        [type]: 'string',
-        [doc]: 'Path or URL to RSS atom.xml',
+        [type]: "string",
+        [doc]: "Path or URL to RSS atom.xml",
         [defaultValue]: null
+    },
+    logo: {
+        [type]: ["string", "object"],
+        [defaultValue]: "/images/logo.png",
+        [doc]:
+            "Path or URL to the website's logo to be shown on the left of the navigation bar or footer"
     }
 };
