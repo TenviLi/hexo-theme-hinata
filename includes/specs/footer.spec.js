0362ecd7409f901b75a52b9e341ad874a75f967d
gylidian <gylidian@gmail.com>
2019-11-24 21:00:59 +0800

    refactor(config): footer specs

--- includes/specs/footer.spec.js
+++ includes/specs/footer.spec.js
@@ -1,24 +1,39 @@
-const { doc, type, defaultValue } = require('../common/utils').descriptors;
+const { doc, type, defaultValue } = require("../common/utils").descriptors;
 
 module.exports = {
-    [type]: 'object',
-    [doc]: 'Footer section link settings',
-    links: {
-        ...require('./icon_link.spec'),
-        [doc]: 'Links to be shown on the right of the footer section',
-        [defaultValue]: {
-            'Creative Commons': {
-                icon: 'fab fa-creative-commons',
-                url: 'https://creativecommons.org/'
-            },
-            'Attribution 4.0 International': {
-                icon: 'fab fa-creative-commons-by',
-                url: 'https://creativecommons.org/licenses/by/4.0/'
-            },
-            'Download on GitHub': {
-                icon: 'fab fa-github',
-                url: 'http://github.com/ppoffice/hexo-theme-icarus'
-            }
+    [type]: "object",
+    [doc]: "Footer section link settings",
+    [defaultValue]: {
+        "Follow Me": {
+            Weibo: "https://www.weibo.com",
+            Zhihu: "https://www.zhihu.com",
+            Instagram: "https://www.instagram.com",
+            Twitter: "https://twitter.com",
+            Github: "https://github.com/gylidian"
+        },
+        Resources: {
+            Designmodo: "//designmodo.com/",
+            Jenkov: "//tutorials.jenkov.com/",
+            "Tympanus.net": "//tympanus.net/codrops/",
+            Uisdc: "//www.uisdc.com/",
+            "scotch.io": "//scotch.io",
+            "css-tricks": "//css-tricks.com"
+        },
+        "My Friends": {
+            Demo1: "https://github.com/gylidian",
+            Demo2: "https://github.com/gylidian",
+            Demo3: "https://github.com/gylidian",
+            Demo4: "https://github.com/gylidian",
+            Demo5: "https://github.com/gylidian"
         }
-    }
+    },
+    '*': {
+        [type]: 'object',
+        [doc]: 'Single column links settings',
+        '*': {
+            [type]: 'string',
+            [doc]: 'Path or URL to the footer column item',
+            [required]: true,
+            [defaultValue]: 'https://github.com/gylidian'
+        },
 };
