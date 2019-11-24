dc439e8d41edf46b508c0c5c312a8160b8ca6d8f
gylidian <gylidian@gmail.com>
2019-11-24 21:06:22 +0800

    feat(helper): add helper `_js`

--- includes/helpers/override.js
+++ includes/helpers/override.js
@@ -2,16 +2,26 @@
  * Helper functions that override Hexo built-in helpers.
  *
  * @example
-*     <%- _js() %>
-*     <%- _css() %>
+ *     <%- _js() %>
+ *     <%- _css() %>
  */
 
-module.exports = function (hexo) {
-    hexo.extend.helper.register('_css', function (url) {
-        const urlFor = hexo.extend.helper.get('url_for').bind(this);
-        if (!url.endsWith('.css') && !url.includes('?')) {
-            url += '.css';
+hexo.extend.helper.register("_js", function(url, defer = false, async = false) {
+    const urlFor = hexo.extend.helper.get("url_for").bind(this);
+    if (!url.endsWith(".js") && !url.includes("?")) {
+        url += ".js";
+    }
+    return `<script src="${urlFor(
+        url
+    )}"${async ? " async" : ""}${defer ? " defer" : ""}></script>`;
+});
+
+module.exports = function(hexo) {
+    hexo.extend.helper.register("_css", function(url) {
+        const urlFor = hexo.extend.helper.get("url_for").bind(this);
+        if (!url.endsWith(".css") && !url.includes("?")) {
+            url += ".css";
         }
         return `<link rel="stylesheet" href="${urlFor(url)}">`;
     });
-}
+};
