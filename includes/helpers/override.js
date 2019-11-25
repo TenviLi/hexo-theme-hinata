df41fe685a93a0a9c042ab95285d7dca28fcc804
gylidian <gylidian@gmail.com>
2019-11-25 14:07:02 +0800

    fix(helpers): fix ReferenceError: hexo is not defined in override.js

--- includes/helpers/override.js
+++ includes/helpers/override.js
@@ -6,17 +6,18 @@
  *     <%- _css() %>
  */
 
-hexo.extend.helper.register("_js", function(url, defer = false, async = false) {
-    const urlFor = hexo.extend.helper.get("url_for").bind(this);
-    if (!url.endsWith(".js") && !url.includes("?")) {
-        url += ".js";
-    }
-    return `<script src="${urlFor(
-        url
-    )}"${async ? " async" : ""}${defer ? " defer" : ""}></script>`;
-});
-
 module.exports = function(hexo) {
+    
+    hexo.extend.helper.register("_js", function(url, defer = false, async = false) {
+        const urlFor = hexo.extend.helper.get("url_for").bind(this);
+        if (!url.endsWith(".js") && !url.includes("?")) {
+            url += ".js";
+        }
+        return `<script src="${urlFor(
+            url
+        )}"${async ? " async" : ""}${defer ? " defer" : ""}></script>`;
+    });
+
     hexo.extend.helper.register("_css", function(url) {
         const urlFor = hexo.extend.helper.get("url_for").bind(this);
         if (!url.endsWith(".css") && !url.includes("?")) {
